848945cc754cb185e8d783014ba3f4b4f906b915
March <1037585097@qq.com>
2020-07-05 21:37:44 +0800

    修改了主页路由

--- src/router.js
+++ src/router.js
@@ -18,7 +18,7 @@ import GoodsComment from './components/goods/GoodsComment.vue'
 // 3. 创建路由对象
 var router = new VueRouter({
   routes: [ // 配置路由规则
-      {path: '/', redirect: HomeContainer},
+      {path: '/', redirect: '/home'},
       {path: '/home', component: HomeContainer },
       {path: '/member', component: MemberContainer },
       {path: '/shopcar', component: ShopcarContainer },
